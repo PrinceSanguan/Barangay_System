e2611bc5f4d68b1923947e2cb6caf4fb803faa82
blindmelon07 <bryandetera07@gmail.com>
2024-10-05 23:45:03 +0800

    added front endededed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1194,13 +1194,13 @@
     </div>
 
     <div class="container mt-4 text-center copyright">
-      <p>© <span>Copyright</span> <strong class="sitename">Append</strong> <span>All Rights Reserved</span></p>
+      <p>© <span>Copyright</span> <strong class="sitename">Ursulum</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://facebook.com/">Barangay Centro</a>
       </div>
     </div>
 
